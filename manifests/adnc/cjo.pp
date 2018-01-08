e4c151b39775562a05fcfb36021f98461a068b39
zyra capili <zcapili@cambridge.org>
2018-01-08 18:00:46 +0800

    commit manifests on roles

---
+++ manifests/adnc/cjo.pp
@@ -0,0 +1,8 @@
+### Class role::adnc::cjo
+# Role to include profiles and modules for CJO
+
+class role::adnc::cjo {
+
+	include ::profile::cjo::application
+
+}
---
+++ manifests/educ/tng.pp
@@ -0,0 +1,11 @@
+### Class role::educ::tng
+# Role to include profiles and modules for TNG
+
+class role::educ::tng {
+
+    include ::php
+    include ::mysql::client
+    include ::profile::tng::admin
+    include ::profile::tng::go
+
+}
