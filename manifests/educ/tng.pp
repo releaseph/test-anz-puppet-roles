566d3cc5551297c6ad96733c9959958a03919763
zyra capili <zcapili@cambridge.org>
2018-02-27 14:27:43 +0800

    clean up

--- manifests/educ/tng.pp
+++ manifests/educ/tng.pp
@@ -1,11 +1,12 @@
 ### Class role::educ::tng
-# Role to include profiles and modules for TNG
+### Role to include profiles and modules for TNG
 
 class role::educ::tng {
 
+    include ::profile::tng::go
+#Commented out due to version and dependency issue
 #    include ::php
 #    include ::mysql::client
-##    include ::profile::tng::admin
-    include ::profile::tng::go
+#    include ::profile::tng::admin
 
 }
